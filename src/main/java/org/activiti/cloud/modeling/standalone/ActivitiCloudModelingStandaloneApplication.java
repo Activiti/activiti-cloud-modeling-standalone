1c2204788a53221dfe1479d4647b7212c7461a55
yoshimoto <yoshimoto@intra-mart.jp>
2019-02-09 12:00:26 +0900

    Update connector

--- src/main/java/org/activiti/cloud/modeling/standalone/ActivitiCloudModelingStandaloneApplication.java
+++ src/main/java/org/activiti/cloud/modeling/standalone/ActivitiCloudModelingStandaloneApplication.java
@@ -1,25 +1,23 @@
 package org.activiti.cloud.modeling.standalone;
 
 import org.activiti.cloud.organization.EnableActivitiOrganization;
-import org.activiti.cloud.process.model.EnableActivitiProcessModel;
-import org.activiti.cloud.services.process.model.jpa.version.ExtendedJpaRepositoryFactoryBean;
+import org.activiti.cloud.services.organization.jpa.version.ExtendedJpaRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = { "org.activiti.cloud.services.organization.jpa",
-        "org.activiti.cloud.services.process.model.jpa" }, repositoryFactoryBeanClass = ExtendedJpaRepositoryFactoryBean.class)
+		"org.activiti.cloud.services.process.model.jpa" }, repositoryFactoryBeanClass = ExtendedJpaRepositoryFactoryBean.class)
 @EntityScan(basePackages = { "org.activiti.cloud.services.process.model.core.model",
-        "org.activiti.cloud.services.organization.entity" })
+		"org.activiti.cloud.services.organization.entity" })
 
 // We need to definitely remove this
 @ComponentScan("org.activiti.cloud")
 @EnableActivitiOrganization
-@EnableActivitiProcessModel
 public class ActivitiCloudModelingStandaloneApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ActivitiCloudModelingStandaloneApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiCloudModelingStandaloneApplication.class, args);
+	}
 }
